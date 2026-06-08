ea2d787dbd1fc5c6eacfb3e9f36b8cf278be604e
khadmy759-ai <khadmy759@gmail.com>
2026-06-08 03:30:07 +0330

    140503180329

--- ShamsAlShamoos03.Shared/Entities/CrewGaurdPlanEvidence01.cs
+++ ShamsAlShamoos03.Shared/Entities/CrewGaurdPlanEvidence01.cs
@@ -1,257 +1,78 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-public class CrewGaurdPlanEvidence01
+public class CrewGaurdPlanEvidence01 : BaseApprovalNavigationEntity
 {
     [Key]
     public string? CrewGaurdPlanEvidence01ID { get; set; }
 
     /// <summary>
-    ///  رجه نام و نشان ش کا کد ملی
+    /// رجه نام و نشان ش کا کد ملی
     /// </summary>
     public string? MelliCode { get; set; }
-    [ForeignKey("MelliCode")]
-    public virtual TblMaster oo_CrewGaurdPesronel { get; set; }
+    [ForeignKey(nameof(MelliCode))]
+    public virtual TblMaster? oo_CrewGaurdPesronel { get; set; }
 
     /// <summary>
-    ///ای دی فعالیت پروازی 
+    /// ای دی فعالیت پروازی
     /// </summary>
     public string? GaurdPlanEvidence01ID { get; set; }
-    [ForeignKey("GaurdPlanEvidence01ID")]
-    public virtual GaurdPlanEvidence01 oo_GaurdPlanEvidence01ID { get; set; }
+    [ForeignKey(nameof(GaurdPlanEvidence01ID))]
+    public virtual GaurdPlanEvidence01? oo_GaurdPlanEvidence01ID { get; set; }
 
     /// <summary>
-    ///ای دی فعالیت پروازی 
+    /// آیا PIC است
     /// </summary>
     public bool? IsPIC { get; set; }
 
+    /// <summary>
+    /// Duty
+    /// </summary>
     public int? Duty { get; set; }
-    [ForeignKey("Duty")]
-    public virtual CategoryGeneral oo_Duty { get; set; }
+    [ForeignKey(nameof(Duty))]
+    public virtual CategoryGeneral? oo_Duty { get; set; }
 
     /// <summary>
-    ///01 کد ملی تأیید کننده
-    /// </summary>
-    public string? UjobPesronel01 { get; set; }
-    [ForeignKey("UjobPesronel01")]
-    public virtual UjobPesronel01 oo_UjobPesronel01 { get; set; }
-
-    /// <summary>
-    ///01 وضعیت تأیید 
-    /// </summary>
-    public int? StatusConfirmation01 { get; set; }
-    [ForeignKey("StatusConfirmation01")]
-    public virtual CategoryGeneral oo_StatusConfirmation01 { get; set; }
-
-    /// <summary>
-    ///02 کد ملی تأیید کننده
-    /// </summary>
-    public string? UjobPesronel02 { get; set; }
-    [ForeignKey("UjobPesronel02")]
-    public virtual UjobPesronel01 oo_UjobPesronel02 { get; set; }
-
-    /// <summary>
-    ///02 وضعیت تأیید 
-    /// </summary>
-    public int? StatusConfirmation02 { get; set; }
-    [ForeignKey("StatusConfirmation02")]
-    public virtual CategoryGeneral oo_StatusConfirmation02 { get; set; }
-
-    /// <summary>
-    ///03 کد ملی تأیید کننده
-    /// </summary>
-    public string? UjobPesronel03 { get; set; }
-    [ForeignKey("UjobPesronel03")]
-    public virtual UjobPesronel01 oo_UjobPesronel03 { get; set; }
-
-    /// <summary>
-    ///03 وضعیت تأیید 
-    /// </summary>
-    public int? StatusConfirmation03 { get; set; }
-    [ForeignKey("StatusConfirmation03")]
-    public virtual CategoryGeneral oo_StatusConfirmation03 { get; set; }
-
-    /// <summary>
-    ///04 کد ملی تأیید کننده
-    /// </summary>
-    public string? UjobPesronel04 { get; set; }
-    [ForeignKey("UjobPesronel04")]
-    public virtual UjobPesronel01 oo_UjobPesronel04 { get; set; }
-
-    /// <summary>
-    ///04 وضعیت تأیید 
-    /// </summary>
-    public int? StatusConfirmation04 { get; set; }
-    [ForeignKey("StatusConfirmation04")]
-    public virtual CategoryGeneral oo_StatusConfirmation04 { get; set; }
-
-    /// <summary>
-    ///05 کد ملی تأیید کننده
-    /// </summary>
-    public string? UjobPesronel05 { get; set; }
-    [ForeignKey("UjobPesronel05")]
-    public virtual UjobPesronel01 oo_UjobPesronel05 { get; set; }
-
-    /// <summary>
-    ///05 وضعیت تأیید 
-    /// </summary>
-    public int? StatusConfirmation05 { get; set; }
-    [ForeignKey("StatusConfirmation05")]
-    public virtual CategoryGeneral oo_StatusConfirmation05 { get; set; }
-
-    /// <summary>
-    ///06 کد ملی تأیید کننده
-    /// </summary>
-    public string? UjobPesronel06 { get; set; }
-    [ForeignKey("UjobPesronel06")]
-    public virtual UjobPesronel01 oo_UjobPesronel06 { get; set; }
-
-    /// <summary>
-    ///06 وضعیت تأیید 
-    /// </summary>
-    public int? StatusConfirmation06 { get; set; }
-    [ForeignKey("StatusConfirmation06")]
-    public virtual CategoryGeneral oo_StatusConfirmation06 { get; set; }
-
-    /// <summary>
-    ///07 کد ملی تأیید کننده
-    /// </summary>
-    public string? UjobPesronel07 { get; set; }
-    [ForeignKey("UjobPesronel07")]
-    public virtual UjobPesronel01 oo_UjobPesronel07 { get; set; }
-
-    /// <summary>
-    ///07 وضعیت تأیید 
-    /// </summary>
-    public int? StatusConfirmation07 { get; set; }
-    [ForeignKey("StatusConfirmation07")]
-    public virtual CategoryGeneral oo_StatusConfirmation07 { get; set; }
-
-    /// <summary>
-    ///08 کد ملی تأیید کننده
-    /// </summary>
-    public string? UjobPesronel08 { get; set; }
-    [ForeignKey("UjobPesronel08")]
-    public virtual UjobPesronel01 oo_UjobPesronel08 { get; set; }
-
-    /// <summary>
-    ///08 وضعیت تأیید 
-    /// </summary>
-    public int? StatusConfirmation08 { get; set; }
-    [ForeignKey("StatusConfirmation08")]
-    public virtual CategoryGeneral oo_StatusConfirmation08 { get; set; }
-
-    /// <summary>
-    ///09 کد ملی تأیید کننده
-    /// </summary>
-    public string? UjobPesronel09 { get; set; }
-    [ForeignKey("UjobPesronel09")]
-    public virtual UjobPesronel01 oo_UjobPesronel09 { get; set; }
-
-    /// <summary>
-    ///09 وضعیت تأیید 
-    /// </summary>
-    public int? StatusConfirmation09 { get; set; }
-    [ForeignKey("StatusConfirmation09")]
-    public virtual CategoryGeneral oo_StatusConfirmation09 { get; set; }
-
-    /// <summary>
-    ///10 کد ملی تأیید کننده
-    /// </summary>
-    public string? UjobPesronel10 { get; set; }
-    [ForeignKey("UjobPesronel10")]
-    public virtual UjobPesronel01 oo_UjobPesronel10 { get; set; }
-
-    /// <summary>
-    ///10 وضعیت تأیید 
-    /// </summary>
-    public int? StatusConfirmation10 { get; set; }
-    [ForeignKey("StatusConfirmation10")]
-    public virtual CategoryGeneral oo_StatusConfirmation10 { get; set; }
-
-    /// <summary>
-    ///11 کد ملی تأیید کننده
-    /// </summary>
-    public string? UjobPesronel11 { get; set; }
-    [ForeignKey("UjobPesronel11")]
-    public virtual UjobPesronel01 oo_UjobPesronel11 { get; set; }
-
-    /// <summary>
-    ///11 وضعیت تأیید 
-    /// </summary>
-    public int? StatusConfirmation11 { get; set; }
-    [ForeignKey("StatusConfirmation11")]
-    public virtual CategoryGeneral oo_StatusConfirmation11 { get; set; }
-
-    /// <summary>
-    ///12 کد ملی تأیید کننده
-    /// </summary>
-    public string? UjobPesronel12 { get; set; }
-    [ForeignKey("UjobPesronel12")]
-    public virtual UjobPesronel01 oo_UjobPesronel12 { get; set; }
-
-    /// <summary>
-    ///12 وضعیت تأیید 
-    /// </summary>
-    public int? StatusConfirmation12 { get; set; }
-    [ForeignKey("StatusConfirmation12")]
-    public virtual CategoryGeneral oo_StatusConfirmation12 { get; set; }
-
-    /// <summary>
-    ///  ترتیب
+    /// ترتیب
     /// </summary>
     public int? Sortable01Id { get; set; }
-    [ForeignKey("Sortable01Id")]
-    public virtual Sortable01 Sortable01 { get; set; }
+    [ForeignKey(nameof(Sortable01Id))]
+    public virtual Sortable01? Sortable01 { get; set; }
 
     /// <summary>
-    /// متن نوتم 01
+    /// متن نوتم‌ها
     /// </summary>
     public string? NOTAMCHECKETitle01 { get; set; }
-
-    /// <summary>
-    /// متن نوتم 02
-    /// </summary>
     public string? NOTAMCHECKETitle02 { get; set; }
-
-    /// <summary>
-    /// متن نوتم 03
-    /// </summary>
     public string? NOTAMCHECKETitle03 { get; set; }
 
     /// <summary>
-    ///  فعال غیر فعال
+    /// فعال / غیر فعال
     /// </summary>
     public bool? IsHaveJob { get; set; }
 
     public string? FlightEvidence01ID { get; set; }
-    [ForeignKey("FlightEvidence01ID")]
+    [ForeignKey(nameof(FlightEvidence01ID))]
     public virtual FlightEvidence01? ooRateFlight { get; set; }
 
     public string? StandCheckWritten01ID { get; set; }
-    [ForeignKey("StandCheckWritten01ID")]
+    [ForeignKey(nameof(StandCheckWritten01ID))]
     public virtual StandCheckWritten01? ooStandCheck { get; set; }
 
     public string? HealthEvidence01ID { get; set; }
-    [ForeignKey("HealthEvidence01ID")]
+    [ForeignKey(nameof(HealthEvidence01ID))]
     public virtual HealthEvidence01? oo_HealthEvidence01 { get; set; }
 
     public string? NoNoticeEvidence01ID { get; set; }
-    [ForeignKey("NoNoticeEvidence01ID")]
+    [ForeignKey(nameof(NoNoticeEvidence01ID))]
     public virtual NoNoticeEvidence01? oo_NoNoticeEvidence01 { get; set; }
 
     public string? EnglishExamEvidence01ID { get; set; }
-    [ForeignKey("EnglishExamEvidence01ID")]
+    [ForeignKey(nameof(EnglishExamEvidence01ID))]
     public virtual EnglishExamEvidence01? oo_EnglishExamEvidence01 { get; set; }
 
     public string? LastfltDateFilnal01 { get; set; }
 
     [NotMapped]
-    public string? DateHealthEvidence01ID
-    {
-        get
-        {
-            return "000dc";
-        }
-    }
+    public string? DateHealthEvidence01ID => "000dc";
 }
--- ShamsAlShamoos03.Shared/Entities/FlightActivityEvidence01.cs
+++ ShamsAlShamoos03.Shared/Entities/FlightActivityEvidence01.cs
@@ -1,220 +1,38 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-public class FlightActivityEvidence01
+public class FlightActivityEvidence01 : BaseApprovalNavigationEntity
 {
     [Key]
     public string? FlightActivityEvidence01ID { get; set; }
 
     /// <summary>
-    ///01 کد ملی ثبت کننده
+    /// 01 کد ملی ثبت کننده
     /// </summary>
     public string? RegisterPesronel01 { get; set; }
-    [ForeignKey("RegisterPesronel01")]
-    public virtual UjobPesronel01 oo_RegisterPesronel01 { get; set; }
+    [ForeignKey(nameof(RegisterPesronel01))]
+    public virtual UjobPesronel01? oo_RegisterPesronel01 { get; set; }
 
     /// <summary>
     /// یگان
     /// </summary>
     public int? UnitID01 { get; set; }
-    [ForeignKey("UnitID01")]
+    [ForeignKey(nameof(UnitID01))]
     public virtual CategoryGeneral? oo_UnitID01 { get; set; }
 
     /// <summary>
-    ///01 کد ملی تأیید کننده
-    /// </summary>
-    public string? PlanUjobPesronel01 { get; set; }
-    [ForeignKey("PlanUjobPesronel01")]
-    public virtual UjobPesronel01 oo_PlanUjobPesronel01 { get; set; }
-
-    /// <summary>
-    ///01 وضعیت تأیید 
-    /// </summary>
-    public int? PlanStatusConfirmation01 { get; set; }
-    [ForeignKey("PlanStatusConfirmation01")]
-    public virtual CategoryGeneral oo_PlanStatusConfirmation01 { get; set; }
-
-    /// <summary>
-    ///01 کد ملی تأیید کننده
-    /// </summary>
-    public string? UjobPesronel01 { get; set; }
-    [ForeignKey("UjobPesronel01")]
-    public virtual UjobPesronel01 oo_UjobPesronel01 { get; set; }
-
-    /// <summary>
-    ///01 وضعیت تأیید 
-    /// </summary>
-    public int? StatusConfirmation01 { get; set; }
-    [ForeignKey("StatusConfirmation01")]
-    public virtual CategoryGeneral oo_StatusConfirmation01 { get; set; }
-
-    /// <summary>
-    ///02 کد ملی تأیید کننده
-    /// </summary>
-    public string? UjobPesronel02 { get; set; }
-    [ForeignKey("UjobPesronel02")]
-    public virtual UjobPesronel01 oo_UjobPesronel02 { get; set; }
-
-    /// <summary>
-    ///02 وضعیت تأیید 
-    /// </summary>
-    public int? StatusConfirmation02 { get; set; }
-    [ForeignKey("StatusConfirmation02")]
-    public virtual CategoryGeneral oo_StatusConfirmation02 { get; set; }
-
-    /// <summary>
-    ///03 کد ملی تأیید کننده
-    /// </summary>
-    public string? UjobPesronel03 { get; set; }
-    [ForeignKey("UjobPesronel03")]
-    public virtual UjobPesronel01 oo_UjobPesronel03 { get; set; }
-
-    /// <summary>
-    ///03 وضعیت تأیید 
-    /// </summary>
-    public int? StatusConfirmation03 { get; set; }
-    [ForeignKey("StatusConfirmation03")]
-    public virtual CategoryGeneral oo_StatusConfirmation03 { get; set; }
-
-    /// <summary>
-    ///04 کد ملی تأیید کننده
-    /// </summary>
-    public string? UjobPesronel04 { get; set; }
-    [ForeignKey("UjobPesronel04")]
-    public virtual UjobPesronel01 oo_UjobPesronel04 { get; set; }
-
-    /// <summary>
-    ///04 وضعیت تأیید 
-    /// </summary>
-    public int? StatusConfirmation04 { get; set; }
-    [ForeignKey("StatusConfirmation04")]
-    public virtual CategoryGeneral oo_StatusConfirmation04 { get; set; }
-
-    /// <summary>
-    ///05 کد ملی تأیید کننده
-    /// </summary>
-    public string? UjobPesronel05 { get; set; }
-    [ForeignKey("UjobPesronel05")]
-    public virtual UjobPesronel01 oo_UjobPesronel05 { get; set; }
-
-    /// <summary>
-    ///05 وضعیت تأیید 
-    /// </summary>
-    public int? StatusConfirmation05 { get; set; }
-    [ForeignKey("StatusConfirmation05")]
-    public virtual CategoryGeneral oo_StatusConfirmation05 { get; set; }
-
-    /// <summary>
-    ///06 کد ملی تأیید کننده
-    /// </summary>
-    public string? UjobPesronel06 { get; set; }
-    [ForeignKey("UjobPesronel06")]
-    public virtual UjobPesronel01 oo_UjobPesronel06 { get; set; }
-
-    /// <summary>
-    ///06 وضعیت تأیید 
-    /// </summary>
-    public int? StatusConfirmation06 { get; set; }
-    [ForeignKey("StatusConfirmation06")]
-    public virtual CategoryGeneral oo_StatusConfirmation06 { get; set; }
-
-    /// <summary>
-    ///07 کد ملی تأیید کننده
-    /// </summary>
-    public string? UjobPesronel07 { get; set; }
-    [ForeignKey("UjobPesronel07")]
-    public virtual UjobPesronel01 oo_UjobPesronel07 { get; set; }
-
-    /// <summary>
-    ///07 وضعیت تأیید 
-    /// </summary>
-    public int? StatusConfirmation07 { get; set; }
-    [ForeignKey("StatusConfirmation07")]
-    public virtual CategoryGeneral oo_StatusConfirmation07 { get; set; }
-
-    /// <summary>
-    ///08 کد ملی تأیید کننده
-    /// </summary>
-    public string? UjobPesronel08 { get; set; }
-    [ForeignKey("UjobPesronel08")]
-    public virtual UjobPesronel01 oo_UjobPesronel08 { get; set; }
-
-    /// <summary>
-    ///08 وضعیت تأیید 
-    /// </summary>
-    public int? StatusConfirmation08 { get; set; }
-    [ForeignKey("StatusConfirmation08")]
-    public virtual CategoryGeneral oo_StatusConfirmation08 { get; set; }
-
-    /// <summary>
-    ///09 کد ملی تأیید کننده
-    /// </summary>
-    public string? UjobPesronel09 { get; set; }
-    [ForeignKey("UjobPesronel09")]
-    public virtual UjobPesronel01 oo_UjobPesronel09 { get; set; }
-
-    /// <summary>
-    ///09 وضعیت تأیید 
-    /// </summary>
-    public int? StatusConfirmation09 { get; set; }
-    [ForeignKey("StatusConfirmation09")]
-    public virtual CategoryGeneral oo_StatusConfirmation09 { get; set; }
-
-    /// <summary>
-    ///10 کد ملی تأیید کننده
-    /// </summary>
-    public string? UjobPesronel10 { get; set; }
-    [ForeignKey("UjobPesronel10")]
-    public virtual UjobPesronel01 oo_UjobPesronel10 { get; set; }
-
-    /// <summary>
-    ///10 وضعیت تأیید 
-    /// </summary>
-    public int? StatusConfirmation10 { get; set; }
-    [ForeignKey("StatusConfirmation10")]
-    public virtual CategoryGeneral oo_StatusConfirmation10 { get; set; }
-
-    /// <summary>
-    ///11 کد ملی تأیید کننده
-    /// </summary>
-    public string? UjobPesronel11 { get; set; }
-    [ForeignKey("UjobPesronel11")]
-    public virtual UjobPesronel01 oo_UjobPesronel11 { get; set; }
-
-    /// <summary>
-    ///11 وضعیت تأیید 
-    /// </summary>
-    public int? StatusConfirmation11 { get; set; }
-    [ForeignKey("StatusConfirmation11")]
-    public virtual CategoryGeneral oo_StatusConfirmation11 { get; set; }
-
-    /// <summary>
-    ///12 کد ملی تأیید کننده
-    /// </summary>
-    public string? UjobPesronel12 { get; set; }
-    [ForeignKey("UjobPesronel12")]
-    public virtual UjobPesronel01 oo_UjobPesronel12 { get; set; }
-
-    /// <summary>
-    ///12 وضعیت تأیید 
-    /// </summary>
-    public int? StatusConfirmation12 { get; set; }
-    [ForeignKey("StatusConfirmation12")]
-    public virtual CategoryGeneral oo_StatusConfirmation12 { get; set; }
-
-    /// <summary>
-    /// منطقه پروازی 
+    /// منطقه پروازی
     /// </summary>
     public int? FLTAREANOTAMCHECK { get; set; }
-    [ForeignKey("FLTAREANOTAMCHECK")]
-    public virtual CategoryGeneral oo_TFLTAREANOTAMCHECK { get; set; }
+    [ForeignKey(nameof(FLTAREANOTAMCHECK))]
+    public virtual CategoryGeneral? oo_TFLTAREANOTAMCHECK { get; set; }
 
     /// <summary>
-    ///  وضعیت هوا
+    /// وضعیت هوا
     /// </summary>
     public int? StatusFlightActivityEvidence01 { get; set; }
-    [ForeignKey("StatusFlightActivityEvidence01")]
-    public virtual CategoryGeneral oo_StatusFlightActivityEvidence01 { get; set; }
+    [ForeignKey(nameof(StatusFlightActivityEvidence01))]
+    public virtual CategoryGeneral? oo_StatusFlightActivityEvidence01 { get; set; }
 
     /// <summary>
     /// تاریخ شروع
@@ -222,34 +40,26 @@ public class FlightActivityEvidence01
     public string? FromDateFlightActivityEvidence01Title { get; set; }
 
     /// <summary>
-    ///  تاریخ خاتمه
+    /// تاریخ خاتمه
     /// </summary>
     public string? ToDateFlightActivityEvidence01Title { get; set; }
 
     /// <summary>
-    ///  ترتیب
+    /// ترتیب
     /// </summary>
     public int? Sortable01Id { get; set; }
-    [ForeignKey("Sortable01Id")]
-    public virtual Sortable01 Sortable01 { get; set; }
+    [ForeignKey(nameof(Sortable01Id))]
+    public virtual Sortable01? Sortable01 { get; set; }
 
     /// <summary>
-    /// متن نوتم 01
+    /// متن نوتم‌ها
     /// </summary>
     public string? NOTAMCHECKETitle01 { get; set; }
-
-    /// <summary>
-    /// متن نوتم 02
-    /// </summary>
     public string? NOTAMCHECKETitle02 { get; set; }
-
-    /// <summary>
-    /// متن نوتم 03
-    /// </summary>
     public string? NOTAMCHECKETitle03 { get; set; }
 
     /// <summary>
-    ///  فعال غیر فعال
+    /// فعال / غیر فعال
     /// </summary>
     public bool? IsHaveJob { get; set; }
 }
