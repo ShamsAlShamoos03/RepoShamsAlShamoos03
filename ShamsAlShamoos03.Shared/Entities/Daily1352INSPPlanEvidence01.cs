ab65264ed0c18c8f4713ab4ae8bf5855c534b3b0
khadmy759-ai <khadmy759@gmail.com>
2026-06-09 17:39:16 +0330

    140503191721

--- ShamsAlShamoos03.Shared/Entities/Daily1352INSPPlanEvidence01.cs
+++ ShamsAlShamoos03.Shared/Entities/Daily1352INSPPlanEvidence01.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-public class Daily1352INSPPlanEvidence01 : BaseTrackingNavigationEntity
+public class Daily1352INSPPlanEvidence01 : BaseDaily1352EvidenceEntity
 {
     [Key]
     public string? Daily1352INSPPlanEvidence01ID { get; set; }
@@ -20,50 +20,4 @@ public class Daily1352INSPPlanEvidence01 : BaseTrackingNavigationEntity
     [ForeignKey("INSPEDID01")]
     public virtual Daily1352INSPListPlanEvidence01 oo_INSPEDID01 { get; set; }
 
-    /// <summary>
-    ///01 کد ملی ثبت کننده
-    /// </summary>
-    public string? RegisterPesronel01 { get; set; }
-    [ForeignKey("RegisterPesronel01")]
-    public virtual UjobPesronel01 oo_RegisterPesronel01 { get; set; }
-
-    /// <summary>
-    ///01 کد ملی ویرایش کننده کننده
-    /// </summary>
-    public string? EditedPesronel01 { get; set; }
-    [ForeignKey("EditedPesronel01")]
-    public virtual UjobPesronel01 oo_EditedPesronel011 { get; set; }
-
-
-    /// <summary>
-    ///  ترتیب
-    /// </summary>
-    public int? Sortable01Id { get; set; }
-    [ForeignKey("Sortable01Id")]
-    public virtual Sortable01 Sortable01 { get; set; }
-
-    /// <summary>
-    /// متن نوتم 01
-    /// </summary>
-    public string? NOTAMCHECKETitle01 { get; set; }
-
-    /// <summary>
-    /// متن نوتم 02
-    /// </summary>
-    public string? NOTAMCHECKETitle02 { get; set; }
-
-    /// <summary>
-    /// متن نوتم 03
-    /// </summary>
-    public string? NOTAMCHECKETitle03 { get; set; }
-
-    /// <summary>
-    /// تاریخ آخرین ویرایش یا ارسال توسط یگان
-    /// </summary>
-    public DateTime? TimeSignatureOREditYagen01 { get; set; }
-
-    /// <summary>
-    ///  فعال غیر فعال
-    /// </summary>
-    public bool? IsHaveJob { get; set; }
 }
